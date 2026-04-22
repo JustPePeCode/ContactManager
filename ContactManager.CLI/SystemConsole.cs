b1b5cb605a5e63ac379efb2e5d85c63cb75d3a2b
JustPePeCode <peterpauldd@hotmail.com>
2026-04-22 16:42:51 +0200

    Backlog Contact Toevoegen

---
+++ ContactManager.CLI/SystemConsole.cs
@@ -0,0 +1,12 @@
+﻿namespace ContactManager.CLI;
+
+using ContactManager.Core;
+
+public class SystemConsole : IConsole
+{
+    public void WriteLine(string message) => Console.WriteLine(message);
+
+    public void Write(string message) => Console.Write(message);
+
+    public string ReadLine() => Console.ReadLine()!;
+}
