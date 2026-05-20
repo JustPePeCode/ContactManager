1653ff1738dee753079f60b3029b44102714a777
JustPePeCode <peterpauldd@hotmail.com>
2026-05-20 11:43:48 +0200

    GetById (utils)

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -3,27 +3,27 @@ import {normalizeSearchText,showElement,hideElement} from './ui-utils.js'
 
 
 
-const contactGrid = document.getElementById("contact-grid")
-const contactList= document.getElementById("contact-list")
+const contactGrid = GetById("contact-grid")
+const contactList= GetById("contact-list")
 
-const searchContactInput = document.getElementById("search-contact-input")
-const searchContactButton = document.getElementById("search-contact-button")
-const addContactButton = document.getElementById("add-contact-button")
+const searchContactInput = GetById("search-contact-input")
+const searchContactButton = GetById("search-contact-button")
+const addContactButton = GetById("add-contact-button")
 
-const addContact = document.getElementById("add-contact")
-const addContactInput = document.getElementById("add-contact-input")
-const addEmailInput = document.getElementById("add-email-input")
-const addGsmInput = document.getElementById("add-gsm-input")
-const addSubmitButton = document.getElementById("add-submit-button")
+const addContact = GetById("add-contact")
+const addContactInput = GetById("add-contact-input")
+const addEmailInput = GetById("add-email-input")
+const addGsmInput = GetById("add-gsm-input")
+const addSubmitButton = GetById("add-submit-button")
 
-const changeContact = document.getElementById("change-contact")
-const changeContactInput = document.getElementById("change-contact-input")
-const changeEmailInput = document.getElementById("change-email-input")
-const changeGsmInput = document.getElementById("change-gsm-input")
-const changeSubmitButton = document.getElementById("change-submit-button")
+const changeContact = GetById("change-contact")
+const changeContactInput = GetById("change-contact-input")
+const changeEmailInput = GetById("change-email-input")
+const changeGsmInput = GetById("change-gsm-input")
+const changeSubmitButton = GetById("change-submit-button")
 
-const removeContact = document.getElementById("remove-contact")
-const removeContactCard = document.getElementById("remove-contact-card")
+const removeContact = GetById("remove-contact")
+const removeContactCard = GetById("remove-contact-card")
 
 let selectedContactId
 
--- Frontend/src/ui-utils.js
+++ Frontend/src/ui-utils.js
@@ -11,3 +11,10 @@ export function hideElement(element){
     element.classList.add("hidden")
 
 }
+
+export function getById(id) {
+    const element = document.getElementById(id);
+    if (element === null)
+        throw new Error(`Could not find element with id '${id}'.`);
+    return element;
+}
