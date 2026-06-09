ee4136dfdf6addfb1f8205fb3f088ce2609ca1f5
JustPePeCode <peterpauldd@hotmail.com>
2026-06-09 16:44:15 +0200

    contactApi TypeScript

--- react-frontend/src/api/contactsApi.ts
+++ react-frontend/src/api/contactsApi.ts
@@ -1,10 +1,9 @@
-getContacts()
-searchContacts(name)
-createContact(request)
-updateContact(id, request)
-deleteContact(id)
-
-import type { Contact } from "../types/contacts";
+import type {
+  Contact,
+  CreateContactRequest,
+  UpdateContactRequest,
+  CreateContactResponse,
+} from "../types/contacts";
 
 export async function getContacts(): Promise<Contact[]> {
   const response = await fetch("/api/contacts");
@@ -16,8 +15,7 @@ export async function getContacts(): Promise<Contact[]> {
   return await response.json();
 }
 
-
-export async function searchContacts(name): Promise<Contact[]> {
+export async function searchContacts(name:string): Promise<Contact[]> {
   const response = await fetch(`/api/contacts/search?name=${name}`);
 
   if (!response.ok) {
@@ -25,4 +23,42 @@ export async function searchContacts(name): Promise<Contact[]> {
   }
 
   return await response.json();
+}
+
+export async function createContact(request: CreateContactRequest): Promise<CreateContactResponse> {
+  const response = await fetch("/api/contacts", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(request),
+  });
+
+  if (!response.ok) {
+    throw new Error("Kon contact niet toevoegen");
+  }
+
+  return await response.json();
+}
+
+export async function updateContact(id :number, request: UpdateContactRequest): Promise<Contact> {
+  const response = await fetch(`/api/contacts/${id}`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(request),
+  });
+
+  if (!response.ok) {
+    throw new Error("Kon contact niet veranderen");
+  }
+
+  return await response.json();
+}
+
+export async function deleteContact(id:number): Promise<void> {
+  const response = await fetch(`/api/contacts/${id}`, {
+    method: "DELETE",
+  });
+
+  if (!response.ok) {
+    throw new Error("Kon contact niet verwijderen");
+  }
 }
