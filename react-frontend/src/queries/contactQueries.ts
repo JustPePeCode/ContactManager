d8cab6ce68e61e8d9a74edec0e3cb35e5172e623
JustPePeCode <peterpauldd@hotmail.com>
2026-06-10 10:54:33 +0200

    contactQueries

--- react-frontend/src/queries/contactQueries.ts
+++ react-frontend/src/queries/contactQueries.ts
@@ -1,5 +1,6 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { getContacts, createContact, searchContacts } from "../api/contactsApi";
+import { getContacts, createContact, searchContacts, updateContact,deleteContact } from "../api/contactsApi";
+import type { UpdateContactRequest } from "../types/contacts";
 
 export function useContacts() {
   return useQuery({
@@ -23,4 +24,24 @@ export function useSearchContacts(name:string){
         queryKey: ["contacts",name],
         queryFn: () =>searchContacts(name)
     });
+}
+export function useUpdateContact(){
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn:({id,request}:{id: number, request:UpdateContactRequest}) => updateContact(id,request),
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["contacts"]})
+        }
+    })
+}
+export function useDeleteContact(){
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn:(id:number) => deleteContact(id),
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["contacts"]})
+        }
+    })
 }
